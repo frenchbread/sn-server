524effb2f38ce1c30bea873d1a701f52d67ca9a7
Damir Mustafin <damir.mustafin@protonmail.ch>
2017-09-29 10:28:53 +0300

    fix

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,6 +6,7 @@ import settingModel from '../models/setting'
 const router = express.Router()
 
 const redirect_uri = 'https://gttdn.io:3031/auth/handleauth/instagram'
+// const redirect_uri = 'http://127.0.0.1:8080/auth/handleauth/instagram'
 
 router.get('/authorize_user/instagram', (req, res) => {
   res.redirect(
@@ -17,13 +18,17 @@ router.get('/authorize_user/instagram', (req, res) => {
 
 router.get('/handleauth/instagram', (req, res) => {
 
+
+
   settingModel.get()
     .then(setting => {
-      if (!setting || !setting.instagram_token) {
+      if (!setting) {
+        console.log('setting doesnot exist')
         // If token does not exist, request new one & save to db
         // and return with success status
         ig.authorize_user(req.query.code, redirect_uri, (err, result) => {
           if (err) {
+            console.log(err)
             res.json({ ok: false })
           } else {
             console.log('Yay! Access token is ' + result.access_token)
@@ -35,6 +40,7 @@ router.get('/handleauth/instagram', (req, res) => {
           }
         })
       } else {
+        console.log('setting exists')
         // If token exists check if it's okay
         // and return with status
         ig.user_self_feed({}, function(err, medias, pagination, remaining, limit) {
