946caacffd5828381c57315ee4e3afaf9260b446
Damir Mustafin <damir.mustafin@protonmail.ch>
2017-10-02 15:05:54 +0300

    Update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,21 @@
 import express from 'express'
 import intexModel from '../models'
+import apiIG from '../api/instagram'
 
 const router = express.Router()
 
 // home
 router.get('/', (req, res) => {
+  apiIG.checkForNewMedia()
+
   res.render('index', intexModel())
+})
+
+router.get('/subs', (req, res) => {
+  const hub = req.query['hub.challenge']
+  console.log(req.query)
+
+  res.send(hub)
 })
 
 module.exports = router
